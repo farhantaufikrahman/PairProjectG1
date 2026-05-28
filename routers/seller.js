e9a1e3cc94ac1fac65f164e4bba2b54e204f0a48
Fakhriadlis <farfakhri04@gmail.com>
2026-05-29 05:42:49 +0700

    pg4

--- routers/seller.js
+++ routers/seller.js
@@ -20,7 +20,9 @@ router.use(function (req, res, next) {
   }
 });
 
+
 router.get("/:usId", Controller.baseSeller);
+router.get("/:usId/showProfile", Controller.showProfile);
 router.get("/:usId/addProduct", Controller.getProduct);
 router.post("/:usId/addProduct", Controller.addProduct);
 router.get("/:usId/edtProduct/:id", Controller.formEdit);
